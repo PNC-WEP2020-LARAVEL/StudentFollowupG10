4f7354b7dc7151c7ad9d5b9385980d2069d72baa
raiya.sous <raiya.sous@PNC.LAN>
2020-03-13 11:28:18 +0700

    login and register

--- app/Role.php
+++ app/Role.php
@@ -3,8 +3,20 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class Role extends Model
 {
-    //
+      /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'role',
+    ];
+
+    public function user(){
+       return $this->hasMany(User::class);
+    }
 }
+
